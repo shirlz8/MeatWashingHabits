9e44ff2a5d0ed055d1e95a64ae6e4ec537c50acb
Shirley Zeng <shirleyzeng8y@gmail.com>
2021-03-15 15:22:56 -0700

    load csv files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//Load data from CSV file asynchronously and render chart
-d3.csv('data/exploratory_data.csv').then((data) => {
+// Exploratory view
+d3.csv('data/exploratory_data.csv').then((exploratory_data) => {
   // data.forEach((d) => {
   //   d.cost = +d.cost;
   //   d.year = +d.year;
@@ -14,5 +14,18 @@ d3.csv('data/exploratory_data.csv').then((data) => {
   //   data
   // );
 
-  console.log(data);
+  console.log(exploratory_data);
+});
+
+// What are people trying to remove view
+d3.csv('data/wash_to_remove_data.csv').then((remove_data) => {
+  console.log(remove_data);
+});
+
+// Reasons view
+d3.csv('data/reasons_for_washing_data.csv').then((reasons_washing_data) => {
+  d3.csv('data/reasons_for_not_washing_data.csv').then((reasons_not_washing_data) => {
+    console.log(reasons_washing_data);
+    console.log(reasons_not_washing_data);
+  });
 });
