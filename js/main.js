4e19f7d9989e36803ae2c7dc0ce0a7f6bcde599c
John <likisongproduct@gmail.com>
2021-03-15 14:57:54 -0700

    data load

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 //Load data from CSV file asynchronously and render chart
-d3.csv('data/disaster_costs.csv').then((data) => {
-  data.forEach((d) => {
-    d.cost = +d.cost;
-    d.year = +d.year;
-    d.date = parseTime(d.mid);
-    // Optional: other data preprocessing steps
-  });
+d3.csv('data/exploratory_data.csv').then((data) => {
+  // data.forEach((d) => {
+  //   d.cost = +d.cost;
+  //   d.year = +d.year;
+  //   d.date = parseTime(d.mid);
+  //   // Optional: other data preprocessing steps
+  // });
+  // const timeline = new Timeline(
+  //   {
+  //     parentElement: '#vis',
+  //     // Optional: other configurations
+  //   },
+  //   data
+  // );
 
-  const timeline = new Timeline(
-    {
-      parentElement: '#vis',
-      // Optional: other configurations
-    },
-    data
-  );
+  console.log(data);
 });
