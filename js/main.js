6d8c704b7c636ffe1ef0bfebcdebcdc84599c00a
Shirley Zeng <shirleyzeng8y@gmail.com>
2021-03-16 15:43:25 -0700

    Renaming data variables to satisfy linter

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Exploratory view
+d3.csv('data/exploratory_data.csv').then((exploratoryData) => {
+  // Initialize the charts
+  const householdSizeBarChart = new HouseholdSizeBarChart({
+    parentElement: '#vis',
+  }, data);
+
+  console.log(exploratoryData);
+});
+
+// What are people trying to remove view
+d3.csv('data/wash_to_remove_data.csv').then((removeData) => {
+  console.log(removeData);
+});
+
+// Reasons view
+d3.csv('data/reasons_for_washing_data.csv').then((reasonsWashingData) => {
+  d3.csv('data/reasons_for_not_washing_data.csv').then((reasonsNotWashingData) => {
+    console.log(reasonsWashingData);
+    console.log(reasonsNotWashingData);
+  });
+});
