854786e05adb0ea0699a064e2ac496c0e2c7c20c
Shirley Zeng <shirleyzeng8y@gmail.com>
2021-03-16 16:06:29 -0700

    Deleting extra main.js

--- js/src/main.js
+++ js/src/main.js
@@ -1,19 +1,5 @@
 // Exploratory view
 d3.csv('data/exploratory_data.csv').then((exploratoryData) => {
-  // data.forEach((d) => {
-  //   d.cost = +d.cost;
-  //   d.year = +d.year;
-  //   d.date = parseTime(d.mid);
-  //   // Optional: other data preprocessing steps
-  // });
-  // const timeline = new Timeline(
-  //   {
-  //     parentElement: '#vis',
-  //     // Optional: other configurations
-  //   },
-  //   data
-  // );
-
   console.log(exploratoryData);
 });
 
@@ -24,10 +10,8 @@ d3.csv('data/wash_to_remove_data.csv').then((removeData) => {
 
 // Reasons view
 d3.csv('data/reasons_for_washing_data.csv').then((reasonsWashingData) => {
-  d3.csv('data/reasons_for_not_washing_data.csv').then(
-    (reasonsNotWashingData) => {
-      console.log(reasonsWashingData);
-      console.log(reasonsNotWashingData);
-    }
-  );
+  d3.csv('data/reasons_for_not_washing_data.csv').then((reasonsNotWashingData) => {
+    console.log(reasonsWashingData);
+    console.log(reasonsNotWashingData);
+  });
 });
