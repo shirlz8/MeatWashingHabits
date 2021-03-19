8df5deaffe08129f548df6d01e81ea7157f0411e
John <likisongproduct@gmail.com>
2021-03-18 23:45:11 -0700

    some initial setup

--- js/src/householdSizeBarChart.js
+++ js/src/householdSizeBarChart.js
@@ -1,176 +1,166 @@
 class HouseholdSizeBarChart {
-    /**
-       * Class constructor with initial configuration
-       * @param {Object}
-       * @param {Array}
-       */
-    constructor(_config, _data) {
-        this.config = {
-            parentElement: _config.parentElement,
-            containerWidth: 300,
-            containerHeight: 200,
-            margin: {
-                top: 25, right: 15, bottom: 20, left: 35,
-            },
-        };
-        this.data = _data;
-        console.log('con vis');
+  /**
+   * Class constructor with initial configuration
+   * @param {Object}
+   * @param {Array}
+   */
+  constructor(_config, _data) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: 300,
+      containerHeight: 200,
+      margin: {
+        top: 25,
+        right: 15,
+        bottom: 20,
+        left: 35,
+      },
+    };
+    this.data = _data;
+    console.log('con vis');
 
-        this.initVis();
-    }
+    this.initVis();
+  }
 
-    /**
-         * We initialize the arc generator, scales, axes, and append static elements
-         */
-    initVis() {
-        const vis = this;
-        vis.tooltipPadding = 10;
+  /**
+   * We initialize the arc generator, scales, axes, and append static elements
+   */
+  initVis() {
+    const vis = this;
 
-        // Calculate inner chart size. Margin specifies the space around the actual chart.
-        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+    console.log('init vis');
 
-        // Define size of SVG drawing area
-        vis.svg = d3.select(vis.config.parentElement).append('svg')
-            .attr('width', vis.config.containerWidth)
-            .attr('height', vis.config.containerHeight);
+    // Calculate inner chart size. Margin specifies the space around the actual chart.
+    vis.width =
+      vis.config.containerWidth -
+      vis.config.margin.left -
+      vis.config.margin.right;
+    vis.height =
+      vis.config.containerHeight -
+      vis.config.margin.top -
+      vis.config.margin.bottom;
 
-        // Append group element that will contain our actual chart
-        // and position it according to the given margin config
-        vis.chartArea = vis.svg.append('g')
-            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+    // Define size of SVG drawing area
+    vis.svg = d3
+      .select(vis.config.parentElement)
+      .append('svg')
+      .attr('width', vis.config.containerWidth)
+      .attr('height', vis.config.containerHeight);
 
-        vis.chart = vis.chartArea.append('g');
+    // Append group element that will contain our actual chart
+    // and position it according to the given margin config
+    vis.chartArea = vis.svg
+      .append('g')
+      .attr(
+        'transform',
+        `translate(${vis.config.margin.left},${vis.config.margin.top})`
+      );
 
-        // Initialize scales
-        vis.xScale = d3.scaleBand()
-            .range([0, vis.width])
-            .paddingInner(0.15);
+    vis.chart = vis.chartArea.append('g');
 
-        vis.yScale = d3.scaleLinear()
-            .range([vis.height, 0]);
+    // Initialize scales
+    vis.xScale = d3.scaleBand().range([0, vis.width]).paddingInner(0.15);
 
-        // Set axis domains
-        vis.xScale.domain(['1', '2', '3', '4', '5', '6']);
+    vis.yScale = d3.scaleLinear().range([vis.height, 0]);
 
-        // Initialize axes
-        vis.xAxis = d3.axisBottom(vis.xScale)
-            .tickFormat((d) => {
-                if (d === '6') return `${d}+`;
-                return d;
-            })
-            .ticks(6);
+    // Set axis domains
+    vis.xScale.domain(['1', '2', '3', '4', '5', '6']);
 
-        vis.yAxis = d3.axisLeft(vis.yScale)
-            .ticks(6)
-            .tickSize(-vis.width);
+    // Initialize axes
+    vis.xAxis = d3
+      .axisBottom(vis.xScale)
+      .tickFormat((d) => {
+        if (d === '6') return `${d}+`;
+        return d;
+      })
+      .ticks(6);
 
-        // Append axis groups
-        vis.xAxisG = vis.chart.append('g')
-            .attr('class', 'axis x-axis')
-            .attr('transform', `translate(0,${vis.height})`);
+    vis.yAxis = d3.axisLeft(vis.yScale).ticks(6).tickSize(-vis.width);
 
-        vis.yAxisG = vis.chart.append('g')
-            .attr('class', 'axis y-axis');
+    // Append axis groups
+    vis.xAxisG = vis.chart
+      .append('g')
+      .attr('class', 'axis x-axis')
+      .attr('transform', `translate(0,${vis.height})`);
 
-        vis.updateVis();
-    }
+    vis.yAxisG = vis.chart.append('g').attr('class', 'axis y-axis');
 
-    /**
-         * Prepare the data and scales before we render it.
-         */
-    updateVis() {
-        const vis = this;
+    vis.updateVis();
+  }
 
-        vis.washCount = d3.rollup(vis.data, v => d3.sum(v, d => {
-            if (meatTypeFilter != "")
-                return d[meatTypeFilter];
-            else return d.Wash_Any;
-        }), d => d.Houshold_size);
-        vis.totalCount = d3.rollup(vis.data, v => v.length, d => d.Houshold_size);
+  /**
+   * Prepare the data and scales before we render it.
+   */
+  updateVis() {
+    const vis = this;
 
-        vis.washCountData = [];
-        vis.washCount.forEach((value, key) => {
-            var row = {
-                householdSize: key,
-                wash: value,
-                dontWash: vis.totalCount.get(key) - value
-            };
-            vis.washCountData.push(row);
-        })
+    vis.washCount = d3.rollup(
+      vis.data,
+      (v) => d3.sum(v, (d) => d.Beef),
+      (d) => d.Houshold_size
+    );
+    vis.totalCount = d3.rollup(
+      vis.data,
+      (v) => v.length,
+      (d) => d.Houshold_size
+    );
 
-        vis.subgroups = ["dontWash", "wash"];
+    vis.washCountData = [];
+    vis.washCount.forEach((value, key) => {
+      const row = {
+        householdSize: key,
+        wash: value,
+        dontWash: vis.totalCount.get(key) - value,
+      };
+      vis.washCountData.push(row);
+    });
 
-        vis.stackedWashCountData = d3.stack()
-            .keys(vis.subgroups)
-            (vis.washCountData);
+    vis.subgroups = ['dontWash', 'wash'];
 
-        vis.yScale.domain([0, d3.max(vis.totalCount.values()) + 200]);
-        vis.renderVis();
-    }
+    vis.stackedWashCountData = d3.stack().keys(vis.subgroups)(
+      vis.washCountData
+    );
 
-    // Return colour of the wash and dont wash categories
-    color(d) {
-        const vis = this;
+    vis.yScale.domain([0, d3.max(vis.totalCount.values()) + 200]);
+    vis.renderVis();
+  }
 
-        const color = d3.scaleOrdinal()
-            .domain(vis.subgroups)
-            .range(["#C1504F", "#4E81BE"]);
+  // Return colour of the wash and dont wash categories
+  color(d) {
+    const vis = this;
 
-        return color(d);
-    }
+    const color = d3
+      .scaleOrdinal()
+      .domain(vis.subgroups)
+      .range(['#C1504F', '#4E81BE']);
 
-    /**
-         * Bind data to visual elements (enter-update-exit) and update axes
-         */
-    renderVis() {
-        const vis = this;
+    return color(d);
+  }
 
-        const bars = vis.chart.selectAll('.bar_g')
-            .data(vis.stackedWashCountData, (d) => d)
-            .join('g')
-            .attr('class', 'bar_g')
-            .attr('fill', (d) => vis.color(d.key))
-            .selectAll('.bar_rect')
-            .data(d => d)
-            .join('rect')
-            .attr('class', (d) => "household" + d.data.householdSize)
-            .attr('width', vis.xScale.bandwidth())
-            .attr('height', (d) => vis.yScale(d[0]) - vis.yScale(d[1]))
-            .attr('y', (d) => vis.yScale(d[1]))
-            .attr('x', (d) => vis.xScale(d.data.householdSize));
+  /**
+   * Bind data to visual elements (enter-update-exit) and update axes
+   */
+  renderVis() {
+    const vis = this;
 
-        bars.on('mouseover', (event, d) => {
-            // Add hover shading
-            d3.selectAll("rect.household" + d.data.householdSize)
-                .style("stroke", 'black')
-                .attr('stroke-width', '2');
+    const bars = vis.chart
+      .selectAll('.bar_g')
+      .data(vis.stackedWashCountData, (d) => d)
+      .join('g')
+      .attr('class', 'bar_g')
+      .attr('fill', (d) => vis.color(d.key))
+      .selectAll('.bar_rect')
+      .data((d) => d)
+      .join('rect')
+      .attr('class', 'bar_rect')
+      .attr('width', vis.xScale.bandwidth())
+      .attr('height', (d) => vis.yScale(d[0]) - vis.yScale(d[1]))
+      .attr('y', (d) => vis.yScale(d[1]))
+      .attr('x', (d) => vis.xScale(d.data.householdSize));
 
-            // Add Tooltip
-            d3.select('#tooltip')
-                .style('display', 'block')
-                .style('left', (event.pageX + vis.tooltipPadding + 'px'))
-                .style('top', (event.pageY + vis.tooltipPadding + 'px'))
-                .html(`
-                    <div>Do not wash : ${d.data.dontWash}</div>
-                    <div>Wash : ${d.data.wash}</div>
-                `);
-        }).on('mouseleave', (event, d) => {
-            // Remove hover shading
-            d3.selectAll("rect.household" + d.data.householdSize)
-                .attr('stroke-width', '0');
+    // render axis
+    vis.xAxisG.call(vis.xAxis).call((g) => g.select('.domain').remove());
 
-            // Remove tooltip
-            d3.select('#tooltip').style('display', 'none');
-        });
-
-        // render axis
-        vis.xAxisG
-            .call(vis.xAxis)
-            .call((g) => g.select('.domain').remove());
-
-        vis.yAxisG
-            .call(vis.yAxis)
-            .call((g) => g.select('.domain').remove());
-    }
+    vis.yAxisG.call(vis.yAxis).call((g) => g.select('.domain').remove());
+  }
 }
